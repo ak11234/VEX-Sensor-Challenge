8ab414519fc7227ee72b1462443b600c53d4596a
ak11234 <akpc556@gmail.com>
2015-07-10 10:13:40 -0500

    Make this usable in ROBOTC

--- Robotrotationcode.c
+++ Robotrotationcode.c
@@ -1,34 +1,36 @@
-int distance;
-while((SensorValue[line1] > 2925 || SensorValue[line2] > 2925) && (SensorValue[line1] > 2925 || SensorValue[line3] > 2925) && (SensorValue[line2] > 2925 || SensorValue[line3] > 2925))
-  {
-    //Do all of the ball collecting stuff
-  }
-while(distance == -1)
-  {
-        zeropointturn(127, -1, true);
-        waitmsec(100);
-        distance = SensorValue[sonic];
-  }
-setmotorspeed(0);
-float y1;
-float y2;
-float y3;
-while (y2>y3 || y2>y1)
-  {
-    zeropointturn(50, -1, true);
-    waitmsec(50);
-    y1 = SensorValue[sonic];
-    waitmsec(50);
-    y2 = SensorValue[sonic];
-    waitmsec(50);
-    y3 = SensorValue[sonic];
-  }
-while (!(SensorValue[sonic] == y2))
-  {
-  zeropointturn(30, -1, false);
-  }
-setmotorspeed(0)
-while (SensorValue[sonic] > 20)
-  {
-    setmotorspeed(80)
-  }
+task main() {
+  int distance;
+  while((SensorValue[line1] > 2925 || SensorValue[line2] > 2925) && (SensorValue[line1] > 2925 || SensorValue[line3] > 2925) && (SensorValue[line2] > 2925 || SensorValue[line3] > 2925))
+    {
+      //Do all of the ball collecting stuff
+    }
+  while(distance == -1)
+    {
+          zeropointturn(127, -1, true);
+          waitmsec(100);
+          distance = SensorValue[sonic];
+    }
+  setmotorspeed(0);
+  float y1;
+  float y2;
+  float y3;
+  while (y2>y3 || y2>y1)
+    {
+      zeropointturn(50, -1, true);
+      waitmsec(50);
+      y1 = SensorValue[sonic];
+      waitmsec(50);
+      y2 = SensorValue[sonic];
+      waitmsec(50);
+      y3 = SensorValue[sonic];
+    }
+  while (!(SensorValue[sonic] == y2))
+    {
+    zeropointturn(30, -1, false);
+    }
+  setmotorspeed(0)
+  while (SensorValue[sonic] > 20)
+    {
+      setmotorspeed(80)
+    }
+}
