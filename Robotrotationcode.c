f189a4c2894438e360af1ce359d83f7b3a1ccc9c
Arie Ogranovich <sorryitsarie@gmail.com>
2015-07-10 10:22:53 -0500

    This is finished

--- Robotrotationcode.c
+++ Robotrotationcode.c
@@ -32,3 +32,22 @@ while (SensorValue[sonic] > 20)
   {
     setmotorspeed(80)
   }
+  while (y2>y3 || y2>y1)
+    {
+      zeropointturn(50, -1, true);
+      waitmsec(50);
+      y1 = SensorValue[sonic];
+      waitmsec(50);
+      y2 = SensorValue[sonic];
+      waitmsec(50);
+      y3 = SensorValue[sonic];
+    }
+while (!(SensorValue[sonic] == y2))
+  {
+    zeropointturn(30, -1, false);
+  }
+setmotorspeed(0)
+while (SensorValue[sonic] > 60)
+  {
+    setmotorspeed(80)
+  }
